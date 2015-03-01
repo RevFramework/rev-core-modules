11e0ca403326637bb3f6bdc9143a18c14dcbfca6
Russell Briggs <russ@paraflyer.net>
2015-03-01 13:44:53 +0000

    Do not cache Rev Views in debug mode

--- rev_app/http_controllers/app.py
+++ rev_app/http_controllers/app.py
@@ -9,13 +9,20 @@ class RevAppHTTPController(FlaskView):
         return render_template('index.html')
     
     def view(self, module, view_id):
-        view = current_app.cache.get('/view'+request.path)
-        if not view:
+        view = None
+        if current_app.debug:
+            # Recompile view every time
             view = current_app.registry.get('View').get_rendered_view(module, view_id)
+        else:
+            # Load view from cache if available
+            view = current_app.cache.get('/view'+request.path)
             if not view:
-                view = '404'
-            current_app.cache.set('/view'+request.path, view)
-        if view == '404':
+                view = current_app.registry.get('View').get_rendered_view(module, view_id)
+                if view:
+                    current_app.cache.set('/view'+request.path, view)
+                else:
+                    current_app.cache.set('/view'+request.path, '404')
+        if not view or view == '404':
             abort(404)
         return view
         
