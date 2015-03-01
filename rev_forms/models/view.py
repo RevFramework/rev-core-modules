62c3e25e910197b694ecdb7a0553e9ef510af9bf
Russell Briggs <russ@paraflyer.net>
2015-03-01 21:58:56 +0000

    Rev Forms now implemented with a <rev-form> tag to avoid confusion  - This also means we can now require the 'model' and 'var' attributes  - Implemented <fieldwrapper> tag, which can be used to easily include custom content in rev-forms

--- rev_forms/models/view.py
+++ rev_forms/models/view.py
@@ -3,6 +3,7 @@ from rev.db.exceptions import XMLImportError
 from . import FORM_WIDGETS
 
 from lxml import etree
+from flask import render_template
 
 FORM_LAYOUTS = ['stacked', 'inline', 'horizontal']
 
@@ -28,40 +29,37 @@ class View(OverrideModel):
                     raise KeyError()
          
         # Locate form tags
-        form_elements = view_xml_tree.xpath('//form')
+        form_elements = view_xml_tree.xpath('//rev-form')
         for form in form_elements:
              
             # Process form attributes
             form_attrs = dict(form.attrib)
+
+            if not 'model' in form_attrs or not 'var' in form_attrs:
+                raise XMLImportError("<rev-form> must have at least 'model' and 'var' attributes.")
+
+            if not self.registry.model_exists(form_attrs['model']):
+                raise XMLImportError("<form> 'model' does not exist: {}".format(form_attrs['model']))
+            form_attrs['name'] = form_attrs['var']+'_form'
              
             if 'layout' in form_attrs:
                 if form_attrs['layout'] not in FORM_LAYOUTS:
                     raise XMLImportError("<form> layout '{}' is invalid. Valid options are: {}".format(form_attrs['layout'], ', '.join(FORM_LAYOUTS)))
             else:
                 form_attrs['layout'] = 'stacked'
+
+            # Transform the rev-form tag into a form tag with appropriate attributes
+            form.tag = 'form'
+            form.attrib['name'] = form_attrs['name']
+            form.attrib['novalidate'] = 'novalidate'
+            if form_attrs['layout'] != 'stacked':
+                if 'class' not in form.attrib:
+                    form.attrib['class'] = 'form-'+form_attrs['layout']
+                else:
+                    form.attrib['class'] += ' form-'+form_attrs['layout']
                  
-            if 'model' in form_attrs:
-                if not self.registry.model_exists(form_attrs['model']):
-                    raise XMLImportError("<form> 'model' does not exist: {}".format(form_attrs['model']))
-                if 'name' not in form_attrs:
-                    raise XMLImportError("<form> elements with 'model' attribute must also have a 'name' attribute")
-                if 'var' not in form_attrs:
-                    raise XMLImportError("<form> elements with 'model' attribute must also have a 'var' attribute")
-                # Augment the form tag with extra attributes required for a model form
-                form.attrib['novalidate'] = 'novalidate'
-                if form_attrs['layout'] != 'stacked':
-                    if 'class' not in form.attrib:
-                        form.attrib['class'] = 'form-'+form_attrs['layout']
-                    else:
-                        form.attrib['class'] += ' form-'+form_attrs['layout']
-            else:
-                form_attrs['model'] = None
-                 
-            # Locate fields
-            field_elements = form.xpath('field')
-            if field_elements and not form_attrs['model']:
-                raise XMLImportError("<field> element found but no 'model' specified on the parent <form>")
-             
+            # Deal with <field> tags
+            field_elements = form.xpath('//field')
             model_obj = self.registry.get(form_attrs['model'])
             for field in field_elements:
                  
@@ -95,3 +93,34 @@ class View(OverrideModel):
                 # Substitute <field> tag with widget html
                 field.getparent().replace(field, widget_xml)
                  
+            # Deal with <fieldwrapper> tags
+            wrapper_elements = form.xpath('//fieldwrapper')
+            for wrapper in wrapper_elements:
+                 
+                # Process wrapper attributes
+                wrapper_attrs = dict(wrapper.attrib)
+                 
+                # Generate wrapper source
+                wrapper_source = render_template('form_widgets/FieldWrapper.j2',
+                                                    form_attrs=form_attrs,
+                                                    wrapper_attrs=wrapper_attrs)
+                try:
+                    wrapper_xml = etree.fromstring(wrapper_source)
+                except Exception as e:
+                    raise XMLImportError("Error parsing output of FieldWrapper.j2 template: {}".format(e))
+                
+                # Wrap the sub-elements of the fieldwrapper tag
+                content_el = wrapper_xml.xpath('//wrappercontent')
+                if len(content_el) != 1:
+                    raise XMLImportError("FieldWrapper.j2 template must include exactly one <wrappercontent> tag")
+                
+                content_parent = content_el[0].getparent()
+                content_idx = content_parent.index(content_el[0])
+                content_parent.remove(content_el[0])
+                new_index = content_idx
+                for sub_el in wrapper:
+                    content_parent.insert(new_index, sub_el)
+                    new_index += 1
+                
+                # Replace the wrapper tag with the templated XML
+                wrapper.getparent().replace(wrapper, wrapper_xml)
